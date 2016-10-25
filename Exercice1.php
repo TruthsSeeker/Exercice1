c6383d53e6ab976f0282668c5aa6de444b4a287e
TruthsSeeker <heinrichloic@gmail.com>
2016-10-25 17:55:30 +0200

    First commit

---
+++ Exercice1.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href = "theme.css">
+    <title> Je progresse </title>
+  </head>
+
+
+  <body>
+    <h1> Bienvenue sur le premier exercice <h1>
+    <div id="imageholder">
+      <button> </button>
+      <!ajouter un form dans le div>
+    </div>
+  </body>
+</html>
---
+++ brouillon/input.php
@@ -0,0 +1,13 @@
+<!#test des form. Page doit être nommée welcome.php ?>
+
+<html>
+<body>
+
+<form action="output.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
